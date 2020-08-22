952bdb47771264c4dbe991c95e866b3a1b03000a
Vipul Daripkar <vipuldaripkar@gmail.com>
2020-08-22 17:58:12 +0100

    Added ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,16 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://p0.pikist.com/photos/267/533/flat-kitchen-meals-food-delicious-gastronomy-eat-recipe-bio.jpg'),
-    new Recipe('Another Test Recipe', 'This is a test', 'https://p0.pikist.com/photos/267/533/flat-kitchen-meals-food-delicious-gastronomy-eat-recipe-bio.jpg')
+    new Recipe('A Test Recipe', 'This is a test', 'https://p0.pikist.com/photos/267/533/flat-kitchen-meals-food-delicious-gastronomy-eat-recipe-bio.jpg',
+    [
+      new Ingredient('Biscuits', 11),
+      new Ingredient('Nuts', 20)
+    ]),
+    new Recipe('Burger', 'Fast Food Bites', 'https://www.maxpixel.net/static/photo/1x/Food-Fries-Burger-Tasty-Fat-Delicious-Hamburger-4333831.jpg',
+    [
+      new Ingredient('Buns', 2),
+      new Ingredient('Meat', 1)
+    ])
   ];
 
   getRecipes(){
